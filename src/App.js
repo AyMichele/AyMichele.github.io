cae2ddd12ca5a7868f721aa8b6a0c491f3e7b8dc
AyMichele <michele.ayadi@gmail.com>
2020-08-07 11:40:46 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+
 
 const appStyle = {
   height: "100vh",
